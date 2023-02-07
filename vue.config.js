ac4cd2d0f624aaa95a608abfeae9a19d221cb219
Yago Pérez Vázquez <yago@safe.global>
2023-02-07 11:47:34 +0100

    Upgrade packages

--- vue.config.js
+++ vue.config.js
@@ -1,42 +1,53 @@
 // var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const webpack = require('webpack');
+const { defineConfig } = require('@vue/cli-service');
+const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 
-const production = process.env.NODE_ENV === 'production'
+const production = process.env.NODE_ENV === 'production';
 
 module.exports = {
   lintOnSave: false,
   devServer: {
-    disableHostCheck: true
+    allowedHosts: 'all',
     // https: true
   },
+  transpileDependencies: true,
   configureWebpack: {
     optimization: {
       splitChunks: {
-        maxSize: 1000000
-      }
+        maxSize: 1000000,
+      },
     },
     plugins: [
-     // new BundleAnalyzerPlugin(),
-      new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
+      // new BundleAnalyzerPlugin(),
+      new webpack.IgnorePlugin({
+        resourceRegExp: /^\.\/locale$/,
+        contextRegExp: /moment$/,
+      }),
     ],
     output: {
-      globalObject: 'this'
+      globalObject: 'this',
     },
     resolve: {
       alias: {
-        "bn.js": path.resolve(__dirname, 'node_modules/bn.js'),
-        "underscore": path.resolve(__dirname, 'node_modules/underscore')
-      }
-    }
+        'bn.js': path.resolve(__dirname, 'node_modules/bn.js'),
+        underscore: path.resolve(__dirname, 'node_modules/underscore'),
+      },
+    },
+  },
+  configureWebpack: {
+    plugins: [new NodePolyfillPlugin()],
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+    },
   },
   // (dev) force Safari not to cache
-  chainWebpack: config => {
+  chainWebpack: (config) => {
     if (process.env.NODE_ENV === 'development') {
-      config
-        .output
-        .filename('[name].[hash].js')
-        .end()
+      config.output.filename('[name].[hash].js').end();
     }
-  }
-}
+  },
+};
